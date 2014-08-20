7b57c0cea36f08600b8ab249730948bf5bcfa613
Juho Vähäkangas <vahakangas@gmail.com>
2014-08-20 11:04:30 +0300

    Tallennus skripti

---
+++ nauhoita.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#
+# GNU General Public License v3
+#
+
+
+_channel=""
+_time=""
+_duration=""
+_file=""
+
+_all_ok=0
+
+function _iso_to_at() {
+    read Y M D <<< ${1//[-: ]/ }
+    echo "${M}/${D}/${Y}"
+}
+
+function parse_to_at() {
+    # $1 muotoa "2014-07-15 12:23"
+    read year clock <<< ${@//}
+    _time="${clock} $(_iso_to_at $year)"
+}
+
+function calculate_time() {
+    read hours minutes <<< ${1//[.]/ }
+    _duration=$((${hours}*60*60+${minutes}*60))
+}
+
+function channel() {
+    case $1 in
+	tv1) _channel="udp://@239.16.116.1:5555" ;;
+	tv2) _channel="udp://@239.16.116.2:5555" ;;
+	mtv3) _channel="udp://@239.16.116.3:5555" ;;
+	nelonen) _channel="udp://@239.16.116.4:5555" ;;
+	fst5) _channel="udp://@239.16.116.5:5555" ;;
+	sub) _channel="udp://@239.16.116.6:5555" ;;
+	urheilu) _channel="udp://@239.16.116.7:5555" ;;
+	liv) _channel="udp://@239.16.116.8:5555" ;;
+	kutonen) _channel="udp://@239.16.116.10:5555" ;;
+	taivas) _channel="udp://@239.16.116.11:5555" ;;
+	fox) _channel="udp://@239.16.116.12:5555" ;;
+	viisi) _channel="udp://@239.16.116.13:5555" ;;
+	jim) _channel="udp://@239.16.116.14:5555" ;;
+
+	help|*) 
+	    _channel="";
+	    echo "
+Kanavavaihtoehdot ovat:
+=======================
+ tv1
+ tv2
+ mtv3
+ nelonen
+ fst5
+ sub
+ urheilu
+ jim
+ kutonen
+ taivas
+ fox
+ viisi
+ 
+";;
+	esac
+}
+
+function write_to_file() {
+    cvlc -q $_channel --run-time=$_duration --intf=dummy --sout file/ts:${1} --sout-all vlc://quit | at $_time 2>&1 >/dev/null &
+#    ffmpeg -loglevel quiet -i ${_kanava} -acodec copy -t ${_kesto} -vcodec copy -y ${1} | at $_aika &  # 2>&1 >/dev/null
+}
+
+while getopts ":c:t:d:f:h" opt; do
+    case $opt in
+	c)
+	    channel $OPTARG;; 
+	t)
+	    parse_to_at $OPTARG;;
+	d) 
+	    calculate_time $OPTARG;;
+	f)     
+#	    check_variables
+	    write_to_file $OPTARG;;
+	h)
+	    echo "
+
+Skripti telkkariohjelmien nauhoituksiin.
+
+(c) Juho Vähäkangas
+
+Käyttö:
+=======
+
+-c help 
+    kertoo mitä kanavavaihtoehtoja on, mikä kanava nauhoitetaan
+-t vuosi-kuukausi-päivä tunnit:minuutit
+    milloin nauhtoitus aloitetaan (atd pitää olla käynnissä)
+-d tunnit.minuutit
+    nauhoituksen kesto (0.30 = 30min, 1.30 = 1h 30min)
+-f tiedostonimi.ts
+    kerrotaan ohjelmalle mihin nauhoitetaan
+
+Esimerkiksi
+   $0 -c tv1 -t '2014-12-24 12:00' -d 0.30 -f julistus.ts
+"
+;;
+
+    esac
+    
+done
